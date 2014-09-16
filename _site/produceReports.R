fa235a3c2c70e326d44fc35af001fbb838e5271e
Andrey Koval <koval.andrey@gmail.com>
2014-09-16 12:01:49 -0700

    must place in the same directory
    in order to use the same yaml

--- _site/produceReports.R
+++ _site/produceReports.R
@@ -6,12 +6,15 @@ path_ghPages<- base::file.path("./gh-pages-setup.Rmd")
 path_toolbox<- base::file.path("./toolbox-install.Rmd")
 path_TipsTricks <- base::file.path("./tips-and-tricks.Rmd")
 path_DrawingLines <- base::file.path("./bits/drawing-lines/Drawing-lines.Rmd")
+# Lab Log
+path_12_09_2014 <- base::file.path("./labLog/12-09-2014.Rmd")
 
 
 #  Define groups of reports 
+labLogs <- c(path_12_09_2014)
 allReports<- c(path_index, path_ghPages,path_toolbox, path_TipsTricks)
 # Place report paths HERE ###########
-buildthese <- (allReports) ##########
+buildthese <- c(allReports, labLogs) ##########
 ####################################
 
 testit::assert("The knitr Rmd files should exist.", base::file.exists(buildthese))
