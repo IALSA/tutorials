7c0e243ba47ae275eb9e554887e07e85badbe28d
Andrey Koval <koval.andrey@gmail.com>
2014-09-19 12:43:19 -0700

    new report

--- _site/produceReports.R
+++ _site/produceReports.R
@@ -8,10 +8,12 @@ path_TipsTricks <- base::file.path("./tips-and-tricks.Rmd")
 path_DrawingLines <- base::file.path("./bits/drawing-lines/Drawing-lines.Rmd")
 # Lab Log
 path_12_09_2014 <- base::file.path("./labLog/12-09-2014.Rmd")
+path_19_09_2014 <- base::file.path("./labLog/19-09-2014.Rmd")
+
 
 
 #  Define groups of reports 
-labLogs <- c(path_12_09_2014)
+labLogs <- c(path_19_09_2014)
 allReports<- c(path_index, path_ghPages,path_toolbox, path_TipsTricks)
 # Place report paths HERE ###########
 buildthese <- c(allReports, labLogs) ##########
