6dd703ba27f8f11e38b1132a0928c0ebd9590533
Andrey Koval <koval.andrey@gmail.com>
2014-10-13 16:35:59 -0700

    new page lab log

--- _site/produceReports.R
+++ _site/produceReports.R
@@ -1,41 +1,42 @@
+# rm(list=ls(all=TRUE))
+# ########## Production of reports from .Rmd files ###
+# 
+# patternToBuild <- "(?<!README)\\.(R){0,1}md$" #Gets all 'Rmd' and 'md' files, that aren't named `README`.
+# pathFilesToBuild <- list.files(full.names=TRUE, recursive=TRUE)
+# pathFilesToBuild <- grep(patternToBuild, pathFilesToBuild, perl=TRUE, value=TRUE)
+# 
+# testit::assert("The knitr Rmd files should exist.", base::file.exists(pathFilesToBuild))
+# for( pathFile in pathFilesToBuild ) {
+#   #   pathMd <- base::gsub(pattern=".Rmd$", replacement=".md", x=pathRmd)
+#   rmarkdown::render(input = pathFile, 
+#                     output_format=c(
+#                       #                        "pdf_document"
+#                       #                       ,"md_document"
+#                       "html_document"
+#                     ),
+#                     clean=TRUE)
+# }
+# 
+# base::system("bundle exec jekyll build")
+
+m(list=ls(all=TRUE))
 
 ########## Production of reports from .Rmd files ###
 #./Vignettes/Questions
-path_index<- base::file.path("./index.Rmd")
-path_ghPages<- base::file.path("./gh-pages-setup.Rmd")
-path_toolbox<- base::file.path("./toolbox-install.Rmd")
-path_TipsTricks <- base::file.path("./tips-and-tricks.Rmd")
-path_DrawingLines <- base::file.path("./bits/drawing-lines/Drawing-lines.Rmd")
-path_LinksAndResources <- base::file.path("./links-and-resources.Rmd")
-# Lab Log
-path_12_09_2014 <- base::file.path("./12-09-2014.Rmd")
-path_19_09_2014 <- base::file.path("./19-09-2014.Rmd")
+path_index <- base::file.path("./index.Rmd")
 path_26_09_2014 <- base::file.path("./26-09-2014.Rmd")
-
-
-#  Define groups of reports 
-labLogs <- c(path_26_09_2014)
-allReports<- c(path_index, path_ghPages,path_toolbox, path_TipsTricks, path_LinksAndResources)
-# Place report paths HERE ###########
-buildthese <- c(allReports) ##########
-# buildthese <- c( labLogs)
-
-####################################
-
-testit::assert("The knitr Rmd files should exist.", base::file.exists(buildthese))
-# Build the reports
-for( pathRmd in buildthese ) {
+pathFilesToBuild <- c(path_26_09_2014)
+testit::assert("The knitr Rmd files should exist.", base::file.exists(pathFilesToBuild))
+for( pathFile in pathFilesToBuild ) {
   #   pathMd <- base::gsub(pattern=".Rmd$", replacement=".md", x=pathRmd)
-  rmarkdown::render(input = pathRmd, 
+  rmarkdown::render(input = pathFile, 
                     output_format=c(
                       #                        "pdf_document"
-                      #                       ,"word_document"
                       #                       ,"md_document"
                       "html_document"
                     ),
-#                     output_dir = "./" ,
-                    #                     output_file=pathMd,
                     clean=TRUE)
 }
+
 base::system("bundle exec jekyll build")
-# base::system("bundle exec jekyll serve")
+# Or run this from the terminal to keep RStudio free to execute it's own stuff: `bundle exec jekyll serve` at localhost:4000
--- produceReports.R
+++ produceReports.R
@@ -1,41 +1,20 @@
-
+rm(list=ls(all=TRUE))
 ########## Production of reports from .Rmd files ###
-#./Vignettes/Questions
-path_index<- base::file.path("./index.Rmd")
-path_ghPages<- base::file.path("./gh-pages-setup.Rmd")
-path_toolbox<- base::file.path("./toolbox-install.Rmd")
-path_TipsTricks <- base::file.path("./tips-and-tricks.Rmd")
-path_DrawingLines <- base::file.path("./bits/drawing-lines/Drawing-lines.Rmd")
-path_LinksAndResources <- base::file.path("./links-and-resources.Rmd")
-# Lab Log
-path_12_09_2014 <- base::file.path("./12-09-2014.Rmd")
-path_19_09_2014 <- base::file.path("./19-09-2014.Rmd")
-path_26_09_2014 <- base::file.path("./26-09-2014.Rmd")
 
+patternToBuild <- "(?<!README)\\.(R){0,1}md$" #Gets all 'Rmd' and 'md' files, that aren't named `README`.
+pathFilesToBuild <- list.files(full.names=TRUE, recursive=TRUE)
+pathFilesToBuild <- grep(patternToBuild, pathFilesToBuild, perl=TRUE, value=TRUE)
 
-#  Define groups of reports 
-labLogs <- c(path_26_09_2014)
-allReports<- c(path_index, path_ghPages,path_toolbox, path_TipsTricks, path_LinksAndResources)
-# Place report paths HERE ###########
-buildthese <- c(allReports) ##########
-# buildthese <- c( labLogs)
-
-####################################
-
-testit::assert("The knitr Rmd files should exist.", base::file.exists(buildthese))
-# Build the reports
-for( pathRmd in buildthese ) {
+testit::assert("The knitr Rmd files should exist.", base::file.exists(pathFilesToBuild))
+for( pathFile in pathFilesToBuild ) {
   #   pathMd <- base::gsub(pattern=".Rmd$", replacement=".md", x=pathRmd)
-  rmarkdown::render(input = pathRmd, 
+  rmarkdown::render(input = pathFile, 
                     output_format=c(
                       #                        "pdf_document"
-                      #                       ,"word_document"
                       #                       ,"md_document"
                       "html_document"
                     ),
-#                     output_dir = "./" ,
-                    #                     output_file=pathMd,
                     clean=TRUE)
 }
+
 base::system("bundle exec jekyll build")
-# base::system("bundle exec jekyll serve")
