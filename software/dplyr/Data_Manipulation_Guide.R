699924667afff6253634d2ae73c312645396c023
Andrey Koval <koval.andrey@gmail.com>
2014-10-20 15:24:40 -0700

    resoved data man old tutorial

--- software/dplyr/Data_Manipulation_Guide.R
+++ software/dplyr/Data_Manipulation_Guide.R
@@ -1,42 +1,44 @@
-#These first few lines run only when the file is run in RStudio, !!NOT when an Rmd/Rnw file calls it!!
+#These first few lines before the first @knitr chunk will be in RStudio, !!NOT when an Rmd/Rnw file calls it!!
 rm(list=ls(all=TRUE))  #Clear the variables from previous runs.
 
-############################
+
 ## @knitr LoadPackages
 require(RODBC)
 require(dplyr)
 require(ggplot2)
 
-############################
+
 ## @knitr DeclareGlobals
 
-############################
+
 ## @knitr LoadData
-dsL<-readRDS("https://github.com/andkov/Longitudinal_Models_of_Religiosity_NLSY97/blob/master/Data/Derived/dsL.rds")
+# Run to fetch the original file online
+# pathData <- "https://github.com/andkov/Longitudinal_Models_of_Religiosity_NLSY97/raw/master/Data/Derived/dsL.csv"
+# dsL <- read.csv(pathData, header = TRUE)
+#        saveRDS(dsL,"./software/demo_data/NLSY97_dsL.rds")
+dsL <- readRDS("./software/demo_data/NLSY97_dsL.rds")
 
-file <- "<a href="https://github.com/andkov/Longitudinal_Models_of_Religiosity_NLSY97/raw/master/Data/Derived/dsL.rds">dsL</a>"
+# pathData <- gzfile("https://github.com/andkov/Longitudinal_Models_of_Religiosity_NLSY97/raw/master/Data/Derived/dsL.rds")
+# dsL <- readRDS(pathData))
+# close(pathData)
 
-raw_data <- readRDS(file)
+# pathData <- xzfile("https://github.com/andkov/Longitudinal_Models_of_Religiosity_NLSY97/blob/master/Data/Derived/dsL.rds")
+# dsL <- readRDS(pathData)
+# close(pathData)
 
 
+# pathData <- gzfile(file="https://github.com/andkov/Longitudinal_Models_of_Religiosity_NLSY97/raw/master/Data/Derived/dsL.rds")
+# dsL <- readRDS(pathData)
 
-url <- "https://github.com/andkov/Longitudinal_Models_of_Religiosity_NLSY97/raw/master/Data/Derived/dsL.csv"
-my_data <- read.csv(url, header = TRUE)
-
-
-url <- "https://github.com/andkov/Longitudinal_Models_of_Religiosity_NLSY97/raw/master/Data/Derived/dsL.rds"
-dsL <- readRDS(url)
-
-############################
 ## @knitr TweakData
 
-############################
+
 ## @knitr AnalysisChunk01
 
-############################
+
 ## @knitr AnalysisChunk02
 
-############################
+
 ## @knitr AnalysisChunk03
 
 
